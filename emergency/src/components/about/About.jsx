99cd999a12127911eea0fe9ea20db1d497782fca
Akhil3630 <84654617+Akhil3630@users.noreply.github.com>
2022-06-15 22:00:36 +0530

    Add files via upload

---
+++ emergency/src/components/about/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './About.css'
+function About() {
+  return (
+    <>
+        <div className="about">
+          <div className="about-container text-center">
+            <div className="container about-container-div">
+            <h1 className="about-h1 text-light">About</h1>
+            <hr className='border-light mt-3'/>
+            <p className="about-p text-light">Emergency never happened by giving any hint.
+             In reality, observing such emergencies and to resolve them is a tough challenge. 
+             Emergency services are government agencies whose job it is to respond quickly to 
+             emergencies when  they arise, including fire-fighter, police, and ambulance service.
+             We need these services which ensure urgent stabilization of the situation and transport 
+             to definitive care. In this project, we have introduced a web site which provides an 
+             overview of all emergency services and which can be accessible by everyone 24*7.
+             The functionalities of the website are focused on providing a direct communication 
+             service, complete and effective, allowing quick and accurate intervention of the 
+             emergency services. Our purpose is to define a platform fully accessible to all users, 
+             regardless of their languages and/or technological knowledge some of the features are 
+             blood donation portal station to the fire department all in one place.</p>  
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default About
